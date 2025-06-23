00c0caa6d241962e338df579f209f7919928e5a5
Aryan Sharma <aryansharma1323@gmail.com>
2025-06-23 09:14:12 +0530

    Fix

--- app/utils.go
+++ app/utils.go
@@ -1,8 +1,10 @@
 package main
 
+// For given request key, check if the version present in request is supported
 func (req Request) ValidApiVersion() bool {
+	apiKey := req.RequestApiKey
 	version := req.RequestApiVersion
-	versionRange, supported := SupportedVersions[version]
+	versionRange, supported := SupportedVersions[apiKey]
 	if supported && version >= versionRange.MinVersion && version <= versionRange.MaxVersion {
 		return true
 	}
